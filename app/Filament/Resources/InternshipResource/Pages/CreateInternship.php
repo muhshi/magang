73348708691e16be2b9cce0147a4b25834808ff3
muhshi <muhshi>
2025-06-30 16:08:05 +0700

    Nonaktifkan sementara notifikasi WhatsApp n update .env.example

--- app/Filament/Resources/InternshipResource/Pages/CreateInternship.php
+++ app/Filament/Resources/InternshipResource/Pages/CreateInternship.php
@@ -43,23 +43,23 @@ class CreateInternship extends CreateRecord
         }
 
         // Kirim WhatsApp ke admin
-        try {
-            $adminNumber = env('ADMIN_WHATSAPP_NUMBER', '6285399590905'); // pastikan pakai format internasional
+        // try {
+        //     $adminNumber = env('ADMIN_WHATSAPP_NUMBER', '6285399590905'); // pastikan pakai format internasional
 
-            WhatsappService::sendInternshipNotification($adminNumber, [
-                'name' => $freshRecord->full_name,
-                'email' => $freshRecord->email,
-                'instansi' => $freshRecord->school_name,
-                'durasi' => $freshRecord->start_date . ' - ' . $freshRecord->end_date,
-                'motivasi' => $freshRecord->motivation,
-                'keterampilan' => $freshRecord->skills,
-                'foto_url' => $freshRecord->photo_file ? public_path("storage/{$freshRecord->photo_file}") : null,
-                'dokumen_url' => $freshRecord->letter_file ? public_path("storage/{$freshRecord->letter_file}") : null,
-                'dokumen_nama' => $freshRecord->letter_file ? basename($freshRecord->letter_file) : null,
-            ]);
-        } catch (\Exception $e) {
-            \Log::error('Gagal kirim WhatsApp magang: ' . $e->getMessage());
-        }
+        //     WhatsappService::sendInternshipNotification($adminNumber, [
+        //         'name' => $freshRecord->full_name,
+        //         'email' => $freshRecord->email,
+        //         'instansi' => $freshRecord->school_name,
+        //         'durasi' => $freshRecord->start_date . ' - ' . $freshRecord->end_date,
+        //         'motivasi' => $freshRecord->motivation,
+        //         'keterampilan' => $freshRecord->skills,
+        //         'foto_url' => $freshRecord->photo_file ? public_path("storage/{$freshRecord->photo_file}") : null,
+        //         'dokumen_url' => $freshRecord->letter_file ? public_path("storage/{$freshRecord->letter_file}") : null,
+        //         'dokumen_nama' => $freshRecord->letter_file ? basename($freshRecord->letter_file) : null,
+        //     ]);
+        // } catch (\Exception $e) {
+        //     \log::error('Gagal kirim WhatsApp magang: ' . $e->getMessage());
+        // }
     }
 
 
