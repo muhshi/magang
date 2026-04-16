7ea64bebf9c3c704888970b7357caf79040838bb
muhshi <muhshi>
2026-04-16 12:04:14 +0700

    fix: rewrite GoogleLogin for Filament v5 using content() and View schema component

--- app/Filament/Pages/Auth/GoogleLogin.php
+++ app/Filament/Pages/Auth/GoogleLogin.php
@@ -3,8 +3,23 @@
 namespace App\Filament\Pages\Auth;
 
 use Filament\Auth\Pages\Login as BaseLogin;
+use Filament\Schemas\Components\Component;
+use Filament\Schemas\Components\RenderHook;
+use Filament\Schemas\Components\View as ViewComponent;
+use Filament\Schemas\Schema;
+use Filament\View\PanelsRenderHook;
 
 class GoogleLogin extends BaseLogin
 {
-    protected string $view = 'filament.pages.auth.google-login';
+    public function content(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                RenderHook::make(PanelsRenderHook::AUTH_LOGIN_FORM_BEFORE),
+                $this->getFormContentComponent(),
+                $this->getMultiFactorChallengeFormContentComponent(),
+                RenderHook::make(PanelsRenderHook::AUTH_LOGIN_FORM_AFTER),
+                ViewComponent::make('filament.pages.auth.google-login-button'),
+            ]);
+    }
 }
